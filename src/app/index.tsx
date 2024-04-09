8ff6ead7d9cb1f1b3d3fc692c29450e5ea2fd673
Henrique Malvar <henriquemalvar@gmail.com>
2024-04-09 00:23:36 -0300

    feat: init project + class 1

---
+++ src/app/index.tsx
@@ -0,0 +1,42 @@
+import { View, Image, StatusBar } from "react-native"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { Link } from "expo-router"
+
+import { colors } from "@/styles/colors"
+
+import { Input } from "@/components/input"
+import { Button } from "@/components/button"
+
+export default function Home() {
+    return (
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <StatusBar barStyle="light-content" />
+
+            <Image
+                source={require("@/assets/logo.png")}
+                className="h-16"
+                resizeMode="contain"
+            />
+
+            <View className="w-full mt-12 gap-3">
+                <Input>
+                    <MaterialCommunityIcons
+                        name="ticket-confirmation-outline"
+                        color={colors.green[200]}
+                        size={20}
+                    />
+                    <Input.Field placeholder="Código do ingresso" />
+                </Input>
+
+                <Button title="Acessar credencial" />
+
+                <Link
+                    href="/register"
+                    className="text-gray-100 text-base font-bold text-center mt-8"
+                >
+                    Ainda não possui ingresso?
+                </Link>
+            </View>
+        </View>
+    )
+}
---
+++ src/app/register.tsx
@@ -0,0 +1,51 @@
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
+import { Link } from "expo-router"
+import { Image, StatusBar, View } from "react-native"
+
+import { colors } from "@/styles/colors"
+
+import { Button } from "@/components/button"
+import { Input } from "@/components/input"
+
+export default function Register() {
+    return (
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <StatusBar barStyle="light-content" />
+
+            <Image
+                source={require("@/assets/logo.png")}
+                className="h-16"
+                resizeMode="contain"
+            />
+
+            <View className="w-full mt-12 gap-3">
+                <Input>
+                    <FontAwesome6
+                        name="user-circle"
+                        color={colors.green[200]}
+                        size={20}
+                    />
+                    <Input.Field placeholder="Nome completo" />
+                </Input>
+
+                <Input>
+                    <MaterialIcons
+                        name="alternate-email"
+                        color={colors.green[200]}
+                        size={20}
+                    />
+                    <Input.Field placeholder="E-mail" keyboardType="email-address" />
+                </Input>
+
+                <Button title="Realizar inscrição" />
+
+                <Link
+                    href="/"
+                    className="text-gray-100 text-base font-bold text-center mt-8"
+                >
+                    Já possui ingresso?
+                </Link>
+            </View>
+        </View>
+    )
+}
